8994111ea340e027054a9a2ccf011fc99215d4c4
gantraxboys <169122032+gantraxboys@users.noreply.github.com>
2024-07-08 03:45:45 +0700

    Update inputquiz.php

--- inputquiz.php
+++ inputquiz.php
@@ -6,6 +6,7 @@ Nama Gedung:
 <option value="gedung A">Gedung A</option>
 <option value="gedung B">Gedung B</option>
 <option value="gedung C">Gedung C</option>
+<option value="gedung D">gedung d</option>
 <option value="Lain-lain">Lain-lain</option>
 </select>
 <br>
@@ -14,6 +15,7 @@ Nama Lantai:
 <option value="lantai 1">Lantai 1</option>
 <option value="lantai 2">Lantai 2</option>
 <option value="lantai 3">Lantai 3</option>
+<option value="lantai 4">lantai 4</option>  
 </select>
 <br>
 <label for="nama_ruang">Nama Ruang:</label><br>
